d3c84fe6a9a864d6f2cffb2477e60803a8d340e6
Optics-Boss <->
2024-10-09 22:40:45 +0200

    Started working on openchan

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  root "articles#index"
+  resources :articles do
+    resources :comments
+  end
+end
