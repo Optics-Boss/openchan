3472173329fdedee9b9d3d749b63ed1f4e8354ff
Optics-Boss <->
2024-10-29 21:34:05 +0100

    Succesfully added a correct form

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,19 @@
 class TopicsController < ApplicationController
+  def new
+    @topic = Topic.new
+  end
+
   def create
     @boards = Board.all()
     @board = Board.find(params[:board_id])
-    @topic = Topic.new(topic_params)
+    # @topic = Topic.new(topic_params)
+    @topic = @board.topics.create(topic_params)
 
-    if @topic.save
+    if @topic.persisted?
+      puts "If"
       redirect_to board_path(@board)
     else
+      puts "Else"
       render "boards/show", status: :unprocessable_entity, locals: {
         boards: @boards,
         board: @board,
